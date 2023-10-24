8dda71b573fd297e250d597f247894bcc54595e5
Anjali Sridhar <anjali@anjalis-mbp.lan>
2023-10-24 11:10:07 -0700

    add ddp config

--- config/bench_gpt2_ddp.py
+++ config/bench_gpt2_ddp.py
@@ -7,3 +7,4 @@ device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
 compile = False # use PyTorch 2.0 to compile the model to be faster
 profile = False # use pytorch profiler, or just simple benchmarking?
+ddp = True # Use DDP for benchmarking
