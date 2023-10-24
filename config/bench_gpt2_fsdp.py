a49b044c85a71bc4d614cc479317ab971b37f387
Anjali Sridhar <anjali@anjalis-mbp.lan>
2023-10-24 16:19:07 -0700

    enable profile

--- config/bench_gpt2_fsdp.py
+++ config/bench_gpt2_fsdp.py
@@ -6,6 +6,6 @@ seed = 1337
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
 compile = False # use PyTorch 2.0 to compile the model to be faster
-profile = False # use pytorch profiler, or just simple benchmarking?
+profile = True # use pytorch profiler, or just simple benchmarking?
 ddp = False # Use DDP for benchmarking
 fsdp = True
